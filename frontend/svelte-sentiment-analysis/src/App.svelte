4e1eef32cef34a2c30dc4f1476e2011c57770aef
Ali Cem Güler <me@alicemgueler.com>
2024-04-07 22:17:46 +0200

    final

--- frontend/svelte-sentiment-analysis/src/App.svelte
+++ frontend/svelte-sentiment-analysis/src/App.svelte
@@ -10,7 +10,7 @@
         sentiment.set('Analyzing...');
 
         try {
-            const response = await fetch('localhost:5000/predict', {
+            const response = await fetch('http://127.0.0.1:5000/predict', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
